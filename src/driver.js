9ec74b7cef5f9a35718c0080942a9eaff7f27ef1
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-19 02:51:40 +0300

    Added driver tests

--- src/driver.js
+++ src/driver.js
@@ -1,6 +1,7 @@
 const Tree = require("./Tree");
 
-const exampleArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const randomArray = (size) =>
+  Array.from({ length: size }, () => Math.floor(Math.random() * 100));
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -14,22 +15,23 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
+const array = randomArray(30);
+const tree = new Tree(array);
 
-const tree = new Tree(exampleArray);
-tree.insert(6346);
-tree.insert(6347);
-tree.insert(6348);
-tree.insert(6349);
-tree.insert(6360);
+console.log(`Balanced: ${tree.isBalanced()}`);
+console.log(`Level Order: ${tree.levelOrder()}`);
+console.log(`Pre Order: ${tree.preOrder()}`);
+console.log(`Post Order: ${tree.postOrder()}`);
+console.log(`In Order: ${tree.inOrder()}`);
 
-tree.delete(8);
-console.log(tree.find(324)); // Node {key: 324, left: null, right: Node}
-console.log(tree.levelOrder()); // 9 4 67 1 5 23 324 3 7 40 6345
-console.log(tree.inOrder()); // 1 3 4 5 7 9 23 40 67 324 6345
-console.log(tree.preOrder()); // 9 4 1 3 5 7 67 23 40 324 6345
-console.log(tree.postOrder()); // 3 1 7 5 4 40 23 6345 324 67 9
-console.log(tree.height(tree.find(9)));
-console.log(tree.depth(tree.find(324)));
-console.log(tree.isBalanced());
+for (let i = 0; i < 10; i++) {
+  tree.insert(Math.floor(Math.random() * 20));
+}
+
+console.log(`Balanced: ${tree.isBalanced()}`);
 tree.reBalance();
-prettyPrint(tree.root);
+console.log(`Balanced: ${tree.isBalanced()}`);
+console.log(`Level Order: ${tree.levelOrder()}`);
+console.log(`Pre Order: ${tree.preOrder()}`);
+console.log(`Post Order: ${tree.postOrder()}`);
+console.log(`In Order: ${tree.inOrder()}`);
