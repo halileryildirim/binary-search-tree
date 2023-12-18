b6f0b781b3f950cebca335f29cbfeb2fe4fc73dc
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2023-12-19 01:57:40 +0300

    Added preOrder and postOrder

--- src/driver.js
+++ src/driver.js
@@ -16,11 +16,11 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const tree = new Tree(exampleArray);
-// tree.insert(40);
-// tree.delete(8);
+tree.insert(40);
+tree.delete(8);
 console.log(tree.find(324));
 console.log(tree.levelOrder());
-console.log(tree.inOrder());
-console.log(tree.preOrder());
-console.log(tree.postOrder());
+console.log(tree.inOrder()); // 1 3 4 5 7 9 23 40 67 324 6345
+console.log(tree.preOrder()); // 9 4 1 3 5 7 67 23 40 324 6345
+console.log(tree.postOrder()); // 3 1 7 5 4 40 23 6345 324 67 9
 prettyPrint(tree.root);
